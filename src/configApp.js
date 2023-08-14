898cf49f430c61f13a002f9b1d650acd8d837fac
komandante999 <dreftware@yandex.ru>
2023-08-14 17:44:20 +0700

    store

---
+++ src/configApp.js
@@ -0,0 +1,2 @@
+export const API_BASE_URL = "https://vue-moire.skillbox.cc/api/";
+export const TIMEOUT = 500;
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,23 @@
 import { createStore } from "vuex";
+import { API_BASE_URL, TIMEOUT } from "@/configApp";
+// import { setTimeout, resolve  } from "core-js";
+import "core-js/stable/promise";
+import axios from "axios";
 
 export default createStore({
   state: {
-    products: []
+    products: null,
+    productsLoadung: false
   },
   getters: {},
   mutations: {},
-  actions: {},
+  actions: {
+    loadProducts(context) {
+      context.state.productsLoadung = true;
+      return new Promise((resolve) => setTimeout(resolve, TIMEOUT)).then(() => {
+        return axios.get(API_BASE_URL + "products");
+      });
+    }
+  },
   modules: {}
 });
