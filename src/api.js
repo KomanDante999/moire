3173396d6f7b98264cff7adf7702e3fb4b7d5b3c
komandante999 <dreftware@yandex.ru>
2023-08-15 10:36:18 +0700

    load product

--- src/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import "core-js/stable/promise";
-
-export const loadProducts = (productsData) => {
-  axios
-    .get("https://vue-moire.skillbox.cc/api/products")
-    .then((response) => (productsData = response.data));
-  console.log(productsData);
-};
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,11 @@
 import { createStore } from "vuex";
 import { API_BASE_URL, TIMEOUT } from "@/configApp";
-// import { setTimeout, resolve  } from "core-js";
 import "core-js/stable/promise";
 import axios from "axios";
 
 export default createStore({
   state: {
-    products: null,
-    productsLoadung: false
+    products: null
   },
   getters: {},
   mutations: {
@@ -17,11 +15,9 @@ export default createStore({
   },
   actions: {
     loadProducts(context) {
-      context.state.productsLoadung = true;
       return new Promise((resolve) => setTimeout(resolve, TIMEOUT)).then(() => {
         return axios.get(API_BASE_URL + "products").then((response) => {
           context.commit("updateProducts", response.data);
-          context.state.productsLoadung = false;
         });
       });
     }
